option to use local hessians, and fixes

pca_compress/hessapprox.py
@@ -135,7 +135,7 @@ def proj_loss_hessian(model, location, batches, mean_samples,
         last_loss = loss
 
         # Uncomment for debugging purposes:
-        # print('fitting step', i, torch.mean(deltas*deltas).item())
+        # print('fitting step', i, last_loss)
 
     return mat, mean
 
@@ -201,16 +201,16 @@ def proj_loss_hessian_local(model, location, batches, mean_samples,
             acts = acts + expanded_mean
             return acts
 
-        outputs, _ = location.forward(inputs, before=before, modify=project_activations)
-        losses = loss_fn(outputs.detach(), outputs)
+        outputs, _ = location.forward(model, inputs, before=before, modify=project_activations)
+        losses = loss_fn(outputs, outputs)
 
-        grads = torch.autograd.grad(losses, zero_projections, retain_graph=True)[0]
-        grads = torch.autograd.grad(grads, zero_projections, grad_outputs=projections)[0]
+        grads = torch.autograd.grad(torch.sum(losses), zero_projections, create_graph=True)[0]
+        grads = torch.autograd.grad(torch.sum(grads * projections), zero_projections)[0]
 
         all_inputs.extend(projections.detach().cpu().numpy())
         all_outputs.extend(grads.detach().cpu().numpy())
 
-    mat = torch.zeros(all_inputs.shape[1], all_inputs.shape[1]).to(mean.device)
+    mat = torch.zeros(len(all_inputs[0]), len(all_inputs[0])).to(mean.device)
     all_inputs = torch.from_numpy(np.array(all_inputs)).to(mat.device)
     all_outputs = torch.from_numpy(np.array(all_outputs)).to(mat.device)
     last_loss = math.inf
@@ -229,7 +229,7 @@ def proj_loss_hessian_local(model, location, batches, mean_samples,
         last_loss = loss
 
         # Uncomment for debugging purposes:
-        # print('fitting step', i, torch.mean(deltas*deltas).item())
+        print('fitting step', i, last_loss)
 
     return mat, mean
 

pca_compress/project.py
@@ -1,6 +1,6 @@
 import torch
 
-from .hessapprox import proj_mse_losses, proj_loss_hessian
+from .hessapprox import proj_mse_losses, proj_loss_hessian, proj_loss_hessian_local
 from .modules import inject_module, wrap_module_projection
 from .stats import activation_stats, activation_grad_stats
 
@@ -55,25 +55,34 @@ def project_module_hessian(model, location, batches, mean_samples, dim,
                            proj_samples=None,
                            rounds=100,
                            rank_loss=False,
+                           local=False,
                            before=False):
     """
     Use an approximate hessian of the loss function for
     activation projections to project the module at the
     given location.
 
-    See proj_loss_hessian() for more information.
+    See proj_loss_hessian() and proj_loss_hessian_local()
+    for more information.
 
     Returns:
         A new location for the replaced module, for
           example pointing to the new nn.Conv2d.
         This location may be nested inside of the old one.
     """
-    matrix, mean = proj_loss_hessian(model, location, batches, mean_samples,
-                                     loss_fn=loss_fn,
-                                     proj_samples=proj_samples,
-                                     rounds=rounds,
-                                     rank_loss=rank_loss,
-                                     before=before)
+    if local:
+        matrix, mean = proj_loss_hessian_local(model, location, batches, mean_samples,
+                                               loss_fn=loss_fn,
+                                               proj_samples=proj_samples,
+                                               rounds=rounds,
+                                               before=before)
+    else:
+        matrix, mean = proj_loss_hessian(model, location, batches, mean_samples,
+                                         loss_fn=loss_fn,
+                                         proj_samples=proj_samples,
+                                         rounds=rounds,
+                                         rank_loss=rank_loss,
+                                         before=before)
     return project_module_eigen(model, location, dim, mean, matrix, before=before)
 
 